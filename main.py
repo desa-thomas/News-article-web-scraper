87f8dd5ba2ba613fc58efd527e39bbe643d71f26
desa-thomas <desa2thomas@gmail.com>
2024-03-17 18:46:29 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get("https://edition.cnn.com/travel/article/scenic-airport-landings-2020/index.html").text;
-soup = BeautifulSoup(html_text, features=  'html.parser')
+def scrapeWebsite (url): 
+    response = requests.get(url)
+    if response.status_code == 200: 
+        soup = BeautifulSoup(response.text, 'html.parser')
+        title = soup.title.string
+        authorName = soup.find(class_ = "byline__name").string
+        paragraphs = soup.find_all(attrs={'data-component-name':'paragraph'})
+        content = ''
 
+        for i in paragraphs: 
+            if i.string != None: 
+                content += (i.string.strip() + '\n\n')
+
+        return title, authorName, content
+    
+    else: 
+        print("Failed to fetch request")
+        return None, None, None
+    
+
+url = "https://edition.cnn.com/travel/article/scenic-airport-landings-2020/index.html"; 
+title, authorname, content = scrapeWebsite(url)
 print('-'*50)
-print(soup.title.string)
+if(title != None and authorname != None and content != None): 
+    print(title)
+    print("By:" + authorname + '\n') 
+    print(content)
 
-authorName = soup.find(class_ = "byline__name")
-print('By: ' + authorName.string, end = '\n\n')
-
-paragraphs = soup.find_all(attrs={'data-component-name':'paragraph'})
-for i in paragraphs: 
-    if i.string != None: 
-        print(i.string.strip(), end = '\n\n')
 
 
